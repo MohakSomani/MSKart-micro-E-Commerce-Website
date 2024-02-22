c55ac4de856c42077c7ea5966496fb9e67d91c84
MohakSomani <mohaksomani@gmail.com>
2024-02-22 19:33:42 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MSKart-micro-E-Commerce-Website/',
+  base: '/MSKart-micro-E-Commerce-Website--React-Firebase/',
   plugins: [react()],
 })
